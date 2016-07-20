fba494cacddd74cd2de78892dde80212e42ef3da
Bojan Joncic <joncicb@gmail.com>
2016-07-20 19:55:54 +0200

    20. jul 2016

---
+++ application/controllers/Admin/UsersController.php
@@ -0,0 +1,157 @@
+<?php
+
+class Admin_UsersController extends Zend_Controller_Action{
+    public function indexAction() {
+        $flashMessenger = $this->getHelper('FlashMessenger');
+
+        $systemMessages = array(
+            'success' => $flashMessenger->getMessages('success'),
+            'errors' => $flashMessenger->getMessages('errors')
+        );
+        $cmsUsersDbTable = new Application_Model_DbTable_CmsUsers();
+        
+        $users = $cmsUsersDbTable->fetchAll()->toArray();
+        
+        $this->view->users = $users;//prosledjujemo prezentacionoj logici
+       
+        $this->view->systemMessages = $systemMessages;
+    }
+    
+    public function addAction() {
+        $request = $this->getRequest(); //objekat koji cuva inputdata podatke unete preko forme to je getter za post podatke
+        $flashMessenger = $this->getHelper('FlashMessenger');
+
+        $systemMessages = array(//niz za poruke o uspesno ili neuspesno unetim podacima u formu
+            'success' => $flashMessenger->getMessages('success'),
+            'errors' => $flashMessenger->getMessages('errors')
+        );
+
+        $form = new Application_Form_Admin_UserAdd();
+
+        //default form data
+        $form->populate(array(
+        ));
+
+
+
+        if ($request->isPost() && $request->getPost('task') === 'save') {//ovo znaci ukoliko je forma pokrenuta da li je form zahtev POST i da li je yahtev pokrenut na formi, asocijativni niz ciji su kljucevi atributi iz polja forme a vrednosti unos korisnika u formu
+            try {
+
+                //check form is valid
+                if (!$form->isValid($request->getPost())) {//validacija forme ukoliko nisu validni/dobri podaci iz forme bacamo exception i idemo na catch
+                    throw new Application_Model_Exception_InvalidInput('Invalid form data was sent for new user');
+                }
+
+                //get form data
+                $formData = $form->getValues(); //ovo treba da se upise u bazu(podaci iz forme)
+
+                $cmsUsersTable = new Application_Model_DbTable_CmsUsers();
+
+                //insert member returns ID of the new member
+                $userId =  $cmsUsersTable->insertUser($formData);
+
+                // do actual task
+                //save to database etc
+                //set system message
+                $flashMessenger->addMessage('User has been saved', 'success'); //u sesiju upisujemo poruku member has been saved
+                //redirect to same or another page
+                $redirector = $this->getHelper('Redirector'); //redirect je samo i uvek get zahtev i nemoze biti post, radi se samo za get metodu
+                $redirector->setExit(true)//ukoliko je uspesan unos u formu redirektujemo na tu stranu admin _members
+                        ->gotoRoute(array(
+                            'controller' => 'admin_users',
+                            'action' => 'index'
+                                ), 'default', true);
+            } catch (Application_Model_Exception_InvalidInput $ex) {
+                $systemMessages['errors'][] = $ex->getMessage();
+            }
+        }
+
+        $this->view->systemMessages = $systemMessages;
+        $this->view->form = $form;
+    }
+    public function editAction() {
+         $request = $this->getRequest(); //dohvatamo request objekat
+         $id = (int) $request->getParam('id'); //iscitavamo parametar id filtriramo ga da bude int
+
+        if ($id <= 0) {
+            
+            
+            //prekida se izvrsavanje programa i prikazuje se "Page not found"
+            throw new Zend_Controller_Router_Exception('Invalid user id: ' . $id, 404);
+        }
+        
+        $loggedInUser = Zend_Auth::getInstance()->getIdentity();
+        if($id == $loggedInUser['id']){
+            //redirect user to edit profile page
+             $redirector = $this->getHelper('Redirector'); 
+                $redirector->setExit(true)
+                        ->gotoRoute(array(
+                            'controller' => 'admin_profile',
+                            'action' => 'edit'
+                                ), 'default', true);
+        }
+        
+        $cmsUsersTable = new Application_Model_DbTable_CmsUsers();
+
+        $user = $cmsUsersTable->getUserById($id);
+
+        if (empty($user)) {
+
+            throw new Zend_Controller_Router_Exception('No user is found with id: ' . $id, 404);
+        }
+        //$this->view->member = $member;//prosledjujemo $membera prezentacionoj logici
+        $flashMessenger = $this->getHelper('FlashMessenger');
+
+        $systemMessages = array(//niz za poruke o uspesno ili neuspesno unetim podacima u formu
+            'success' => $flashMessenger->getMessages('success'),
+            'errors' => $flashMessenger->getMessages('errors')
+        );
+
+        $form = new Application_Form_Admin_UserEdit($user['id']);
+
+        //default form data
+        $form->populate($user);
+        
+
+
+        if ($request->isPost() && $request->getPost('task') === 'update') {//ovo znaci ukoliko je forma pokrenuta da li je form zahtev POST i da li je yahtev pokrenut na formi, asocijativni niz ciji su kljucevi atributi iz polja forme a vrednosti unos korisnika u formu
+            try {
+
+                //check form is valid
+                if (!$form->isValid($request->getPost())) {//validacija forme ukoliko nisu validni/dobri podaci iz forme bacamo exception i idemo na catch
+                    throw new Application_Model_Exception_InvalidInput('Invalid form data was sent for user');
+                }
+
+                //get form data
+                $formData = $form->getValues(); //ovo treba da se upise u bazu(podaci iz forme)
+                //die(print_r($formData, true));
+                //$cmsMembersTable = new Application_Model_DbTable_CmsMembers();
+                //$cmsMembersTable->insert($formData);
+
+                //Radimo update postojeceg zapisa u tabeli
+               
+                $cmsUsersTable->updateUser($user['id'], $formData);
+
+                // do actual task
+                //save to database etc
+                //set system message
+                $flashMessenger->addMessage('User has been updated', 'success'); //u sesiju upisujemo poruku member has been saved
+                //redirect to same or another page
+                $redirector = $this->getHelper('Redirector'); //redirect je samo i uvek get zahtev i nemoze biti post, radi se samo za get metodu
+                $redirector->setExit(true)//ukoliko je uspesan unos u formu redirektujemo na tu stranu admin _members
+                        ->gotoRoute(array(
+                            'controller' => 'admin_users',
+                            'action' => 'index'
+                                ), 'default', true);
+            } catch (Application_Model_Exception_InvalidInput $ex) {
+                $systemMessages['errors'][] = $ex->getMessage();
+            }
+        }
+
+        $this->view->systemMessages = $systemMessages;
+        $this->view->form = $form;
+        $this->view->user = $user;
+    }
+    }
+
+
--- application/models/DbTable/CmsUsers.php
+++ application/models/DbTable/CmsUsers.php
@@ -2,6 +2,8 @@
 
 class Application_Model_DbTable_CmsUsers extends Zend_Db_Table_Abstract
     {
+    const DEFAULT_PASSWORD ='cubesphp';
+    
     const STATUS_ENABLED = 1;
     const STATUS_DISABLED = 0;
     
@@ -28,6 +30,19 @@ class Application_Model_DbTable_CmsUsers extends Zend_Db_Table_Abstract
         }
     }
     /**
+     * 
+     * @param array $user Associative array with keys at column names and values as column new values
+     * @return int ID of new user
+     */
+    public function insertUser($user) {
+     
+    //set default password for new user   
+     $user['password']=md5(self::DEFAULT_PASSWORD);
+     
+     return $this->insert($user);   
+    }
+
+    /**
      * @param type $id
      * @param array $user Associative array with keys at column names and values as column new values
      */
@@ -37,11 +52,8 @@ class Application_Model_DbTable_CmsUsers extends Zend_Db_Table_Abstract
             //forbid changing of user id
             unset($user['id']);
         }
-        
-        
         $this->update($user, 'id = ' . $id);
-        
-        
+         
     }
     /**
      * @param type int $id
