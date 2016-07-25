4d8a8fe5662a458e7c606adb8e8bb61a3a9f31b5
Bojan Joncic <joncicb@gmail.com>
2016-07-25 20:02:52 +0200

    25. jul 2016 AJAX primer forme sa brendovima i modelima automobila

--- application/controllers/Admin/TestController.php
+++ application/controllers/Admin/TestController.php
@@ -3,16 +3,93 @@
 
 class Admin_TestController extends Zend_Controller_Action
 {
-    public function indexAction(){
+    public function indexAction() {
         die();
         
     }
-    public function jsintroAction(){
+    public function jsintroAction() {
         
     }
-    public function jqueryAction(){
+    public function jqueryAction() {
         
     }
-    
-    
+    public function ajaxintroAction() {
+        
+    }
+    public function ajaxbrandsAction() {
+
+        $brands = array(
+	'fiat' => array(
+		'punto' => 'Punto',
+		'stilo' => 'Stilo',
+		'500l' => '500 L'
+	),
+	'opel' => array(
+		'corsa' => 'Corsa',
+		'astra' => 'Astra',
+		'vectra' => 'Vectra',
+		'insignia' => 'Insignia'
+	),
+	'renault' => array(
+		'twingo' => 'Twingo',
+		'clio' => 'Clio',
+		'megane' => 'Megane',
+		'scenic' => 'Scenic'
+	)
+        );
+        $brandsJson = array();
+        
+        foreach ($brands as $brand => $models){
+            $brandsJson[] = array(
+                'value' => $brand,
+                'label' => ucfirst($brand)
+            );
+         
+        }
+        
+        //disable layout
+       // Zend_Layout::getMvcInstance()->disableLayout();
+        
+        //disable view script rendering
+      //  $this->getHelper('ViewRenderer')->setNoRender(true);
+        
+        //set content type  as json instead of html
+      //  header('Content-Type: application/json');
+        
+     //   echo json_encode($brandsJson);
+        
+     $this->getHelper('Json')->sendJson($brandsJson);//ovaj helper radi sve ovo prethodno
+        
+    }
+    public function ajaxmodelsAction() {
+
+        $brands = array(
+	'fiat' => array(
+		'punto' => 'Punto',
+		'stilo' => 'Stilo',
+		'500l' => '500 L'
+	),
+	'opel' => array(
+		'corsa' => 'Corsa',
+		'astra' => 'Astra',
+		'vectra' => 'Vectra',
+		'insignia' => 'Insignia'
+	),
+	'renault' => array(
+		'twingo' => 'Twingo',
+		'clio' => 'Clio',
+		'megane' => 'Megane',
+		'scenic' => 'Scenic'
+	)
+        );
+        
+        $request -= $this->getRequest();
+        
+        $brand = $request->getParam('brand');
+        
+        if(!isset($brands[$brand])) {
+            throw new Zend_Controller_Router_Exception('Unknown brand', 404);
+        }
+        
+    }
 }
